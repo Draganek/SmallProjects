82a8aa7b8adeb5e3a9fb6c9720c46dd8b037ecf8
Radosław Draganek <100842378+Draganek@users.noreply.github.com>
2024-11-21 22:41:33 +0100

    Add files via upload

---
+++ notifications.js
@@ -0,0 +1,19 @@
+//comomnJS
+/*
+module.exports = {
+    success: (text) => console.log(`Sukces! ${text}`)
+}
+*/
+
+//ES modules
+/* 
+// es module
+// const success = (text) => console.log('Sukces! ' + text);
+
+// export {
+//   success
+// };
+// export default {
+//   success
+// }
+*/
